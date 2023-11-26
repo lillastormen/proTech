266276ef7348fe43cff0534e2ac9fd1fb15f5e4e
hugosandsjo <hugosandsjo@gmail.com>
2023-11-26 16:49:16 +0100

    working on navbar animation

--- script.js
+++ script.js
@@ -15,6 +15,43 @@
 
 // Animate the navbar when scrolling//
 
+// let lastScrollPosition = 0;
+
+// window.addEventListener("scroll", function () {
+//   const navbar = document.querySelector(".desktop nav");
+//   const topBanner = document.querySelector(".topBanner");
+//   const currentScrollPosition = window.scrollY;
+
+//   if (currentScrollPosition > lastScrollPosition) {
+//     navbar.style.transform = "translateY(-120%)"; // Move the navbar outside the page when scrolling down
+//     if (currentScrollPosition > topBanner.offsetHeight) {
+//       topBanner.style.display = "none"; // Hide the topBanner when it's outside the viewport
+//     }
+//   } else {
+//     navbar.style.transform = "translateY(0)"; // Move the navbar to the top of the page
+//     if (currentScrollPosition <= topBanner.offsetHeight) {
+//       topBanner.style.display = "block"; // Show the topBanner when it's in the viewport
+//     }
+//   }
+
+//   lastScrollPosition = currentScrollPosition;
+// });
+
+// let lastScrollPosition = 0;
+
+// window.addEventListener("scroll", function () {
+//   const navbar = document.querySelector(".desktop nav");
+//   const currentScrollPosition = window.scrollY;
+
+//   if (currentScrollPosition > lastScrollPosition) {
+//     navbar.style.transform = "translateY(-120%)"; // Move the navbar outside the page when scrolling down
+//   } else {
+//     navbar.style.transform = "translateY(0)"; // Move the navbar to the top of the page
+//   }
+
+//   lastScrollPosition = currentScrollPosition;
+// });
+
 let lastScrollPosition = 0;
 
 window.addEventListener("scroll", function () {
@@ -23,15 +60,13 @@ window.addEventListener("scroll", function () {
   const currentScrollPosition = window.scrollY;
 
   if (currentScrollPosition > lastScrollPosition) {
-    navbar.style.transform = "translateY(-120%)"; // Move the navbar outside the page when scrolling down
     if (currentScrollPosition > topBanner.offsetHeight) {
-      topBanner.style.display = "none"; // Hide the topBanner when it's outside the viewport
+      navbar.style.transform = "translateY(-150%)"; // Move the navbar less outside the page when the topBanner is not in view
+    } else {
+      navbar.style.transform = "translateY(-120%)"; // Move the navbar outside the page when scrolling down
     }
   } else {
     navbar.style.transform = "translateY(0)"; // Move the navbar to the top of the page
-    if (currentScrollPosition <= topBanner.offsetHeight) {
-      topBanner.style.display = "block"; // Show the topBanner when it's in the viewport
-    }
   }
 
   lastScrollPosition = currentScrollPosition;
